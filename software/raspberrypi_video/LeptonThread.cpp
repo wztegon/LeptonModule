37b7efb1c60743f22e7f238f1524f8996852c1a6
王震 <wangzhentegon@qq.com>
2020-10-29 11:50:45 +0800

    test

--- software/raspberrypi_video/LeptonThread.cpp
+++ software/raspberrypi_video/LeptonThread.cpp
@@ -129,11 +129,13 @@ void LeptonThread::run()
 		int segmentNumber = -1;
 		for(int j=0;j<PACKETS_PER_FRAME;j++) {
 			//if it's a drop packet, reset j to 0, set to -1 so he'll be at 0 again loop
-			read(spi_cs0_fd, result+sizeof(uint8_t)*PACKET_SIZE*j, sizeof(uint8_t)*PACKET_SIZE);
+			int res = read(spi_cs0_fd, result+sizeof(uint8_t)*PACKET_SIZE*j, sizeof(uint8_t)*PACKET_SIZE);
 			int packetNumber = result[j*PACKET_SIZE+1];
 			//查看包数是否正确
+			
 			std::cout <<"packetNumber :" << packetNumber << std::endl;
 			std::cout <<"resets :" << resets << std::endl;
+			std::cout <<"res :" << res << std::endl;
 			if(packetNumber != j) {
 				j = -1;
 				resets += 1;
